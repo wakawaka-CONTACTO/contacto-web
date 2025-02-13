c8fce8b92c9eed22c8062bc9db307087334f85c0
rlarlgnszx <rlarlgnszx@naver.com>
2025-02-13 19:07:02 +0900

    [feat] 서버 연결

--- config.ts
+++ config.ts
@@ -1,5 +1,5 @@
 export const config = {
-  BASE_URL: "http://localhost",
-  CHAT_URL: "http://localhost:8084/chat-websocket"
+  BASE_URL: "https://api.contacto.p-e.kr",
+  CHAT_URL: "https://chat.contacto-chat.p-e.kr/chat-websocket"
 }
 
